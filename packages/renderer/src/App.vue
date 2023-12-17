66b3ef0ecd1ca6c668a21c17d1f824bc5467d5cf
Ari Romero <ari.romero@gmail.com>
2023-12-17 20:11:48 -0300

    update ui

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
 import {BeakerIcon} from '@heroicons/vue/24/solid';
-//import './style.css';
 </script>
 
 <template>
-  <div>
-    <BeakerIcon class="h-16 w-16 text-zinc-500" />
-    <p>...</p>
+  <div class="m-10 p-10 text-center">
+    <BeakerIcon class="h-36 w-36 text-lime-400 mx-auto my-4" />
+    <h1 class="text-3xl mb-6">Electron Starter App</h1>
+    <p>With Vue / Tailwind / Heroicons</p>
   </div>
 </template>
