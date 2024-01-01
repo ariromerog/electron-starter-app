50fa70eada808b7805531230f345eb799c2a8e42
Ari Romero <ari.romero@gmail.com>
2023-12-31 22:42:08 -0300

    added a base layout

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,15 +1,49 @@
 <script lang="ts" setup>
 import {BeakerIcon} from '@heroicons/vue/24/solid';
 import ReactiveCounter from '/@/components/ReactiveCounter.vue';
+import BaseLayout from '/@/layouts/BaseLayout.vue';
 </script>
 
 <template>
-  <div class="m-10 p-10 text-center">
-    <BeakerIcon class="h-36 w-36 text-lime-400 mx-auto my-4" />
-    <h1 class="text-3xl mb-6">Electron Starter App</h1>
-    <p>With Vue / Tailwind / Heroicons</p>
+  <BaseLayout title="Main App Title">
+    <div class="m-10 p-10 text-center">
+      <BeakerIcon class="h-36 w-36 text-lime-400 mx-auto my-4" />
+      <h1 class="text-3xl mb-6">Electron Starter App</h1>
+      <p>With Vue / Tailwind / Heroicons</p>
+      <p>
+        <ReactiveCounter />
+      </p>
+    </div>
+
     <p>
-      <ReactiveCounter />
+      Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint
+      ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
+      officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+      minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla
+      sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia
+      voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia
+      eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris
+      sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
     </p>
-  </div>
+    <p>
+      Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint
+      ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
+      officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+      minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla
+      sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia
+      voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia
+      eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris
+      sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint
+      ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
+      officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+      minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla
+      sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia
+      voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia
+      eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris
+      sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+    </p>
+  </BaseLayout>
 </template>
---
+++ packages/renderer/src/layouts/BaseLayout.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import {BeakerIcon} from '@heroicons/vue/24/solid';
+defineProps(['title']);
+</script>
+
+<template>
+  <div class="main-container">
+    <div class="header bg-gray-800 p-2 shadow-xl">
+      <h1>
+        <BeakerIcon class="h-6 w-6 text-lime-500 float-left mx-2" />
+        {{ title }}
+      </h1>
+    </div>
+    <div class="content">
+      <slot />
+    </div>
+    <hr />
+  </div>
+</template>
+
+<style lang="css">
+.main-container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.header {
+}
+
+.content {
+  height: 100%;
+  overflow: auto;
+}
+</style>
