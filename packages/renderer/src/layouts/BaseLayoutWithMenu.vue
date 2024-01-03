3a6a4b6d1c40666a730e9d7295ed83657b5292a5
Ari Romero <ari.romero@gmail.com>
2024-01-02 23:12:08 -0300

    made the menu collapsable

--- packages/renderer/src/layouts/BaseLayoutWithMenu.vue
+++ packages/renderer/src/layouts/BaseLayoutWithMenu.vue
@@ -1,31 +1,44 @@
 <script lang="ts" setup>
-import {BeakerIcon} from '@heroicons/vue/24/solid';
+import {ref} from 'vue';
+import {CommandLineIcon} from '@heroicons/vue/24/solid';
 defineProps(['title']);
+const isActive = ref(false);
+
+function toggleMenu(): void {
+  isActive.value = !isActive.value;
+}
 </script>
 
 <template>
   <div
     id="header"
-    class="bg-stone-900 text-gray-200 p-2 shadow-md"
+    class="bg-gradient-to-r from-zinc-900 to-zinc-800 text-gray-200 p-2 shadow-md"
   >
     <h1>
-      <BeakerIcon class="h-6 w-6 text-lime-500 float-left mx-2" />
+      <CommandLineIcon
+        class="h-6 w-6 text-lime-500 float-left mr-4 cursor-pointer"
+        @click="toggleMenu"
+      />
       {{ title }}
     </h1>
   </div>
   <div
     id="menu"
-    class="from-stone-800 to-stone-900 bg-gradient-to-r"
+    :class="{active: isActive}"
+    class="bg-zinc-900 bg-gradient-to-r border-r-zinc-800 shadow-sm"
   >
     menu
   </div>
-  <div id="content">
+  <div
+    id="content"
+    class="bg-zinc-900"
+  >
     <slot />
   </div>
 
   <div
     id="footer"
-    class="bg-stone-600 text-center text-sm"
+    class="bg-lime-600 text-center text-white text-sm"
   >
     footer
   </div>
@@ -56,9 +69,14 @@ defineProps(['title']);
   top: 40px;
   left: 0;
   bottom: 20px;
-  width: 100px;
+  width: 99px;
   overflow: auto;
   z-index: 100;
+  transition: 0.3s ease-in width;
+}
+#menu.active {
+  width: 300px;
+  transition: 0.3s ease-out width;
 }
 
 #footer {
